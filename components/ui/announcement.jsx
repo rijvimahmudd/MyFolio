74e16f9049600464696f31cb41270b4408ca55a9
rijvimahmudd <93908058+rijvimahmudd@users.noreply.github.com>
2024-02-10 19:40:26 +0600

    fix the simple bugs on announcement and layout

--- components/ui/announcement.jsx
+++ components/ui/announcement.jsx
@@ -1,41 +1,43 @@
 "use client"
 import { cn } from '@/lib/utils';
-import { Cross1Icon } from '@radix-ui/react-icons'
-import React, { useEffect, useState } from 'react'
+import { Cross1Icon } from '@radix-ui/react-icons';
+import React, { useEffect, useState } from 'react';
 
 const Announcement = () => {
-    const [isClosed, setIsClosed] = useState(false);
-    const hasVisitedBefore = sessionStorage.getItem('hasVisited');
+    const [isClosed, setIsClosed] = useState(true);
 
     useEffect(() => {
-        // Check if the user has visited before by looking for a cookie
+        if (typeof window !== 'undefined') {
+            // Check if the user has visited before in this session
+            const hasVisitedBefore = sessionStorage.getItem('hasVisited');
 
-        // If the user hasn't visited before, show the announcement
-        if (!hasVisitedBefore) {
-            setIsClosed(false);
-        } else {
+            // If the user hasn't visited before in this session, show the announcement
+            if (!hasVisitedBefore) {
+                setIsClosed(false);
+            } else {
+                setIsClosed(true);
+            }
+        }
+    }, []);
+
+    if(isClosed) return null
+
+    const handleClose = () => {
+        if (typeof window !== 'undefined') {
+            // Set a flag indicating that the user has visited in this session
+            sessionStorage.setItem('hasVisited', 'true');
             setIsClosed(true);
         }
-    }, [hasVisitedBefore]);
-    
-
-    if (hasVisitedBefore) {
-        return null;
-    }
-    const handleClose = () => {
-        // Set a cookie indicating that the user has visited
-        sessionStorage.setItem('hasVisited', 'true');
-        setIsClosed(true);
     };
 
     return (
         <div className={cn(
-            "bg-teal-500/70 text-neutral-50 px-4 py-1 text-[0.6rem] md:py-2 md:text-[.8rem] md:px-8 md:text-center relative transition font-bold",
+            "bg-teal-500/70 text-neutral-50 px-4 py-1 text-[0.6rem] md:py-2 md:text-[.8rem] md:px-8 text-center relative transition",
             isClosed && "hidden"
         )}>
             <p>This website is under development. Some features may not work properly.</p>
             <div className='absolute top-1/2 -translate-y-1/2 right-4' onClick={handleClose}>
-                <Cross1Icon className='size-3 md:size-4 inline'/>
+                <Cross1Icon className='w-4 h-4 inline'/>
             </div>
         </div>
     );
